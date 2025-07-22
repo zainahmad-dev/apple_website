4ce58402cadf8658ef7763305bd068513a41bd90
Zain Ahmad <zainahmad4708@gmail.com>
2025-07-22 22:08:18 +0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 
+
 const  App = () => {
 
   return (
@@ -9,6 +10,7 @@ const  App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      
     </main>
   )
 }
---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,25 @@
+
+import { hightlightsSlides } from '../constants'
+
+
+const VideoCarousel = () => {
+  return (
+   <>
+   <div className='flex items-center'>
+      {hightlightsSlides.map((list,i)=>(
+        <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+          <div className='video-carousel_container'>
+        <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+         <video>
+          <source src={list.video} type="video/mp4"/>
+         </video>
+        </div>
+          </div>
+        </div>
+      ))}
+   </div>
+   </>
+  )
+}
+
+export default VideoCarousel

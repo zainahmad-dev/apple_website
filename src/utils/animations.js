3014d1b622d1e552a75090dbae632766062057b9
Zain Ahmad <zainahmad4708@gmail.com>
2025-07-27 09:00:03 +0500

    initial commit

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,23 @@
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger);
+export const animateWithGsap = (target, animationProps,scrollprops)=>{
+   gsap.to(target,{
+    ...animationProps,
+    scrollTrigger:{
+        trigger:target,
+        toggleActions:'restart reverse restart reverse',
+        start:'top 85%',
+        ...scrollprops,
+
+    }
+   }) 
+}
+
+
+
+
+
 export const animateWithGsapTimeline = (timeline,rotationRef, rotationState,firstTarget,SecondTarget,animationProps) =>{
     timeline.to(rotationRef.current.rotation,{
         y: rotationState,
